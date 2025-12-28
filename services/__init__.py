b0299a97ff0f9d7ed5b8515949d1a85b13855729
Brandjuh <Brandjuh@users.noreply.github.com>
2025-12-28 20:18:28 +0100

    Delete services directory

--- services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Services layer for PoliceChief
-"""
-
-from .content_loader import ContentLoader
-from .game_engine import GameEngine
-from .tick_engine import TickEngine
-
-__all__ = ["ContentLoader", "GameEngine", "TickEngine"]
